a008489caf6239c11aedbf86e68af15397940d70
Milagros Bussio <mdbussio@uc.cl>
2023-02-01 13:42:38 -0300

    Se agrega funcionalidad de botón de grilla

--- script.js
+++ script.js
@@ -2,31 +2,60 @@
 
 const contenedor = document.querySelector('.contenedor');
 
-let id = 0;
+function grilla32(){
 
-for (let i = 0; i < 32; i++){
-    const fila = document.createElement('div');    
-    fila.classList.add('fila');
-    fila.style.display = "flex";
-    contenedor.appendChild(fila);
-    for (let j = 0; j < 32; j++){
-        const cuadrito = document.createElement('div');    
-        cuadrito.classList.add('cuadrito');
-        cuadrito.style.width="20px";
-        cuadrito.style.height="20px";
-        cuadrito.style.backgroundColor="white";
-        cuadrito.style.margin = "0px 0px 0px 0px";
-        cuadrito.style.padding = 0;
-        cuadrito.setAttribute('id', id);
-        fila.appendChild(cuadrito);
-        id++
+    let id = 0;
+
+    for (let i = 0; i < 32; i++){
+        const fila = document.createElement('div');    
+        fila.classList.add('fila');
+        fila.style.display = "flex";
+        contenedor.appendChild(fila);
+        for (let j = 0; j < 32; j++){
+            const cuadrito = document.createElement('div');    
+            cuadrito.classList.add('cuadrito');
+            cuadrito.style.width="20px";
+            cuadrito.style.height="20px";
+            cuadrito.style.backgroundColor="white";
+            cuadrito.style.margin = "0px 0px 0px 0px";
+            cuadrito.style.padding = 0;
+            cuadrito.setAttribute('id', id);
+            fila.appendChild(cuadrito);
+            id++
+        };
     };
 };
 
+function grilla16(){
+
+    let id = 0;
+
+    for (let i = 0; i < 16; i++){
+        const fila = document.createElement('div');    
+        fila.classList.add('fila');
+        fila.style.display = "flex";
+        contenedor.appendChild(fila);
+        for (let j = 0; j < 16; j++){
+            const cuadrito = document.createElement('div');    
+            cuadrito.classList.add('cuadrito');
+            cuadrito.style.width="40px";
+            cuadrito.style.height="40px";
+            cuadrito.style.backgroundColor="white";
+            cuadrito.style.margin = "0px 0px 0px 0px";
+            cuadrito.style.padding = 0;
+            cuadrito.setAttribute('id', id);
+            fila.appendChild(cuadrito);
+            id++
+        };
+    };
+};
+
+grilla32();
+
 // PARA CREAR TODOS LOS CUADRITOS COMO OBJETOS 
 
-const cuadritos = document.querySelectorAll(".cuadrito");
-
+let cuadritos = document.querySelectorAll(".cuadrito");
+console.log(cuadritos);
 
 // PARA BORARR 
 
@@ -59,10 +88,34 @@ btnColor.addEventListener('click', () => {
     if (btnColor.textContent == "¡A color!"){
         btnColor.textContent = "B&N";
     } else {
-        btnColor.textContent = "¡A color!"
-    }
-    }
-);
+        btnColor.textContent = "¡A color!";
+    };
+});
+
+// PARA CAMIAR TAMAÑO DE GRILLA
+
+function cambiarGrilla() {
+    // esto borra la grilla
+    const grilla = document.getElementById("grilla");
+    while (grilla.firstChild) {
+        grilla.removeChild(grilla.lastChild);
+    };
+    cuadritos = "";
+    // aca le cambiamos el nombre al botón y creamos la nueva grilla según corresponda
+    if (btnGrilla.textContent == "16x16"){
+        btnGrilla.textContent = "32x32";
+        console.log(cuadritos);
+        grilla16();
+    } else {
+        grilla32();
+        btnGrilla.textContent = "16x16";       
+    };
+    
+    cuadritos = document.querySelectorAll(".cuadrito");;
+}
+
+const btnGrilla = document.querySelector('#btnGrilla');
+btnGrilla.addEventListener('click', cambiarGrilla);
     
 // PARA QUE CUANDO PASE POR ENCIMA DE UN CUADRITO, CONSIGA EL ID Y CON ESO CAMBIE EL ESTILO
 
