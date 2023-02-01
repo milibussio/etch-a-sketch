952ee68d88e388a0fd3298a95ed535584f6f4920
Milagros Bussio <mdbussio@uc.cl>
2023-02-01 10:09:13 -0300

    Se crea funcionalidad
    Se crea funcionalidad para crear todos los cubitos de la grilla como objetos y cambiar si fondo al pasar un mousever mediante el id desde el windows. Se cambia además la grilla de 16x16 a 32x32 y se alarga uno de los bordes.

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
+// PARA CREAR LA CUADRILLA DEL SKETCH
+
 const contenedor = document.querySelector('.contenedor');
 
-let i = 0;
+let id = 0;
 
-while (i < 16){
+for (let i = 0; i < 32; i++){
     const fila = document.createElement('div');    
     fila.classList.add('fila');
-//    fila.style.border = "thick solid #0000FF";
     fila.style.display = "flex";
-//    fila.style.width="500px";
     contenedor.appendChild(fila);
-    i++
-    let j = 0;
-    while (j < 16){
+    for (let j = 0; j < 32; j++){
         const cuadrito = document.createElement('div');    
         cuadrito.classList.add('cuadrito');
-//        cuadrito.src = "images/icono.ico"; 
-//        cuadrito.style.border = "solid black";
-//        cuadrito.style.borderBlockWidth = "1px";
-        cuadrito.style.width="40px";
-        cuadrito.style.height="40px";
+        cuadrito.style.width="20px";
+        cuadrito.style.height="20px";
         cuadrito.style.backgroundColor="white";
-//        cuadrito.style.margin = "0px -3px 0px 0px";
-    cuadrito.style.margin = "0px 0px 0px 0px";
-    cuadrito.style.padding = 0;
+        cuadrito.style.margin = "0px 0px 0px 0px";
+        cuadrito.style.padding = 0;
+        cuadrito.setAttribute('id', id);
         fila.appendChild(cuadrito);
-        j++
-    }
-}
+        id++
+    };
+};
+
+// PARA CREAR TODOS LOS CUADRITOS COMO OBJETOS 
+
+const cuadritos = document.querySelectorAll(".cuadrito");
+ 
+// PARA QUE CUANDO PASE POR ENCIMA DE UN CUADRITO, CONSIGA EL ID Y CON ESO CAMBIE EL ESTILO
+
+window.addEventListener('mouseover', function(e){
+    cuadritos[e.fromElement['id']].style.backgroundColor= "black";   
+});
